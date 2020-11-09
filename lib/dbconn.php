055e28cd2ab630909d9c355ed3fad1c578430d80
ohhajin91 <ohglet@naver.com>
2020-11-09 14:27:51 +0900

    dbconn.php 파일 수정

--- lib/dbconn.php
+++ lib/dbconn.php
@@ -1,5 +1,5 @@
 <?
-    $connect=mysql_connect( "localhost", "ohhajin", "dbwjd2004*") or  
+    $connect=mysql_connect( "localhost", "", "") or  
         die( "SQL server에 연결할 수 없습니다."); 
 
     mysql_select_db("ohhajin",$connect);
